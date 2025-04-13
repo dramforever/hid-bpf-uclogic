dd2ea990969f2c4c2d6b8104e535f132464e337d
dramforever <dramforever@live.com>
2025-04-13 22:30:35 +0800

    First commit

---
+++ src/mk_template.h
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#define DescriptorTemplate_begin \
+        static const struct DESCRIPTOR_NAME { \
+                __u8 _bytes[sizeof (__u8[]){
+
+#define FIELD(_type, _name) \
+                }]; \
+                _type _name; \
+                __u8 _bytes_after_##_name[sizeof (__u8[]){
+
+#define DescriptorTemplate_end \
+                }]; \
+        } __attribute__((packed)) DESCRIPTOR_NAME =
+
+#define DescriptorTemplate(...) \
+        DescriptorTemplate_begin \
+        __VA_ARGS__ \
+        DescriptorTemplate_end
+
+#include DESCRIPTOR_FILE
+
+#undef _descriptor_type
+#undef DescriptorTemplate_begin
+#undef DescriptorTemplate_end
+
+#undef DescriptorTemplate
+#undef FIELD
+
+#define DescriptorTemplate(...) \
+        { ._bytes = { __VA_ARGS__ } };
+#define FIELD(_type, _name) \
+        }, ._name = (_type)-1, ._bytes_after_##_name = {
+
+#include DESCRIPTOR_FILE
+
+#undef DescriptorTemplate
+#undef FIELD
+
+#undef DESCRIPTOR_NAME
+#undef DESCRIPTOR_FILE
---
+++ src/uclogic_v2.in.h
@@ -0,0 +1,119 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+DescriptorTemplate(
+	UsagePage_Digitizers
+	Usage_Dig_Digitizer
+	CollectionApplication(
+		ReportId(VENDOR_REPORT_ID)
+		Usage_Dig_Stylus
+		CollectionPhysical(
+			LogicalMinimum_i8(0)
+			LogicalMaximum_i8(1)
+			ReportSize(1)
+			Usage_Dig_TipSwitch
+			Usage_Dig_BarrelSwitch
+			Usage_Dig_SecondaryBarrelSwitch
+			ReportCount(3)
+			Input(Var|Abs)
+
+			ReportCount(4) // Padding
+			Input(Const)
+
+			Usage_Dig_InRange
+			ReportCount(1)
+			Input(Var|Abs)
+
+			ReportSize(24)
+			ReportCount(1)
+			PushPop(
+				UsagePage_GenericDesktop
+				Unit(in)
+				UnitExponent(-3)
+
+				LogicalMinimum_i32(0)
+				0x27, FIELD(__u32, lmax_x) // LogicalMaximum_i32
+				PhysicalMinimum_i32(0)
+				0x47, FIELD(__u32, pmax_x) // PhysicalMaximum_i32
+				Usage_GD_X
+				Input(Var|Abs)
+
+				LogicalMinimum_i32(0)
+				0x27, FIELD(__u32, lmax_y) // LogicalMaximum_i32
+				PhysicalMinimum_i32(0)
+				0x47, FIELD(__u32, pmax_y) // PhysicalMaximum_i32
+				Usage_GD_Y
+				Input(Var|Abs)
+			)
+
+			LogicalMinimum_i16(0)
+			0x26, FIELD(__u16, lmax_pressure) // LogicalMaximum_i16
+			Usage_Dig_TipPressure
+			ReportSize(16)
+			ReportCount(1)
+			Input(Var|Abs)
+
+			ReportSize(8)
+			ReportCount(2)
+			PushPop(
+				Unit(deg)
+				UnitExponent(0)
+				LogicalMinimum_i8(-60)
+				PhysicalMinimum_i8(-60)
+				LogicalMaximum_i8(60)
+				PhysicalMaximum_i8(60)
+				Usage_Dig_XTilt
+				Usage_Dig_YTilt
+				Input(Var|Abs)
+			)
+		)
+	)
+	UsagePage_GenericDesktop
+	Usage_GD_Keypad
+	CollectionApplication(
+		ReportId(PAD_REPORT_ID)
+		LogicalMinimum_i8(0)
+		LogicalMaximum_i8(1)
+		UsagePage_Digitizers
+		Usage_Dig_TabletFunctionKeys
+		CollectionPhysical(
+			// Byte 1 in report - just exists so we get to be a tablet pad
+			Usage_Dig_BarrelSwitch
+			ReportCount(1)
+			ReportSize(1)
+			Input(Var|Abs)
+			ReportCount(7)
+			Input(Const)
+			// Bytes 2/3 in report - just exists so we get to be a tablet pad
+			UsagePage_GenericDesktop
+			Usage_GD_X
+			Usage_GD_Y
+			ReportCount(2)
+			ReportSize(8)
+			Input(Var|Abs)
+		)
+
+		UsagePage_Button
+		UsageMinimum_i8(1)
+		0x29, FIELD(__u8, num_btn_misc_1) // UsageMaximum_i8
+		0x95, FIELD(__u8, num_btn_misc_2) // ReportCount
+		ReportSize(1)
+		Input(Var|Abs)
+
+#ifdef use_btn_gamepad
+		PushPop(
+			UsagePage_GenericDesktop
+			Usage_GD_Gamepad
+			CollectionApplication(
+				UsagePage_Button
+				UsageMinimum_i8(1)
+				0x29, FIELD(__u8, num_btn_gamepad_1) // UsageMaximum_i8
+				0x95, FIELD(__u8, num_btn_gamepad_2) // ReportCount
+				Input(Var|Abs)
+			)
+		)
+#endif
+
+		0x95, FIELD(__u8, num_btn_padding) // ReportCount
+		Input(Const)
+	)
+)
