eb870c6dcee15e0cb099db57c1ef9192b9d19c62
dramforever <dramforever@live.com>
2025-05-05 12:10:01 +0800

    Add flake package and overlay

--- flake.nix
+++ flake.nix
@@ -42,5 +42,69 @@
           }
         ) { }
       );
+
+      overlays.default = final: prev: {
+        hid-bpf-uclogic = final.callPackage (
+          {
+            lib,
+            rustPlatform,
+            zlib,
+            elfutils,
+            libbpf,
+            pkg-config,
+            makeWrapper,
+            buildPackages,
+            huion-switcher,
+          }:
+
+          let
+            cargoToml = with builtins; fromTOML (readFile ./Cargo.toml);
+
+          in
+          rustPlatform.buildRustPackage {
+            pname = cargoToml.package.name;
+            version = cargoToml.package.version;
+
+            src = ./.;
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            nativeBuildInputs = [
+              pkg-config
+              makeWrapper
+              buildPackages.llvmPackages.clang-unwrapped
+            ];
+
+            buildInputs = [
+              zlib
+              elfutils
+              libbpf
+            ];
+
+            doCheck = false;
+
+            postInstall = ''
+              wrapProgram "$out/bin/"* \
+                --set PATH ${lib.makeBinPath [ huion-switcher ]}
+            '';
+
+            meta.mainProgram = "hid-bpf-uclogic";
+          }
+        ) { };
+      };
+
+      packages = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ self.overlays.default ];
+          };
+        in
+        {
+          inherit (pkgs) hid-bpf-uclogic;
+          default = pkgs.hid-bpf-uclogic;
+        }
+      );
     };
 }
